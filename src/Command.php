29d4615c430e401e3f79b8c8c2753cb5a833c979
ReenExe <ipourri@gmail.com>
2016-01-11 15:59:16 +0200

    progress

---
+++ src/Command.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace ReenExe\DeliveryLimitModel;
+
+class Command
+{
+
+}
--- tests/BlankTest.php
+++ tests/BlankTest.php
@@ -1,9 +1,11 @@
 <?php
 
+use ReenExe\DeliveryLimitModel\Command;
+
 class BlankTest extends \PHPUnit_Framework_TestCase
 {
     public function test()
     {
-
+        new Command();
     }
 }
